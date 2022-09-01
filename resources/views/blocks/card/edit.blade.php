b4a273cca84396fcfd15ebd193f6c53be9feed0a
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-09-01 09:15:42 +0100

    Hide Image and Action for now

--- resources/views/blocks/card/edit.blade.php
+++ resources/views/blocks/card/edit.blade.php
@@ -1,12 +1,16 @@
 <div class="card">
 
-    <img class="card-image-top" style="width: 100%; height: 175px; sbackground: grey"/>
-
+    {{-- <div>
+        <x-forms-fields-croppie name="{{ $name }}[image]" :value="$value->image ?? null" wrapper="none" label="" previewScale="1" height="175" xwidth="400"/>
+    </div> --}}
+    
     <div class="card-body">
         
+        <div>
         <h4>
             <x-forms-fields-wysiwyg :value="$value->title ?? ''" placeholder="Card Title" label="" wrapper="none" name="{{$name}}[title]" toolbar="basic"/>
         </h4>
+        </div>
         {{-- <h4 class="card-title" contenteditable="true">
             TITLE
         </h4> --}}
@@ -14,14 +18,12 @@
         <div class="card-text">
             <x-forms-fields-wysiwyg :value="$value->body ?? ''"  placeholder="Card Text" label="" wrapper="none" name="{{$name}}[body]" toolbar="basic"/>
         </div>
-
+{{-- 
         <div class="card-action">
 
             <button class="btn btn-primary button">Action...</button>
 
-        </div>
+        </div> --}}
     </div>
 
-
 </div>
-
