b9cc907d7e5ea0c630b737b4617e4a7a22772eb2
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-09-27 12:56:57 +0100

    parallax test

--- resources/views/components/element/show.blade.php
+++ resources/views/components/element/show.blade.php
@@ -1,3 +1,28 @@
-<div class="pb-element pb-{{ str_replace('.', ' ', $value->t) ?? '' }} {{ $value->o->css_classes ?? '' }}" id="elm-{{ $unid }}">
+<div class="pb-element pb-{{ str_replace('.', ' ', $value->t) ?? '' }} {{ $value->o->css_classes ?? '' }}" id="elm-{{ $unid }}"
+
+    @isset($value->s->background_image)
+     {{-- ($img = ) ?? false) --}}
+        data-android-fix="false" 
+        class="parallax-window" 
+        data-parallax="scroll" 
+        data-image-src="/storage/{{ $value->s->background_image->hashed_filename }}"
+        @php unset($value->s->background_image); @endphp
+    @endif
+    
+
+
+    >
+
+    {{-- @dump($value) --}}
+     {{-- NOT mobile AND Set to do Parallax --}}
+     {{-- @php
+     $img = \AscentCreative\CMS\Models\File::find($row->styles->background_image);
+ @endphp
+
+ data-android-fix="false" 
+ class="parallax-window" 
+ data-parallax="scroll" 
+ data-image-src="/storage/{{ $img->filepath }}" --}}
+
     @yield('content')
 </div>
